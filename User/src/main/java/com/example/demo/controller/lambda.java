22e70bb85456731cd1894394336f2245400a0e65
machuang <358017672@qq.com>
2019-06-12 11:10:57 +0800

    多线程操作实例

--- User/src/main/java/com/example/demo/controller/lambda.java
+++ User/src/main/java/com/example/demo/controller/lambda.java
@@ -1,5 +1,38 @@
 package com.example.demo.controller;
 
-public class lambda {
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+/**
+ * 【多线程操作实例】
+ * mc/2019年6月12日
+ */
+public class lambda {
+    private final static int a = 0;
+
+    public static void main(String[] args) {
+        lambda.Move();
+    }
+
+    private static void Move() {
+
+        ExecutorService executorService = null;
+        Integer rows = 200;
+        Integer total = 10000;
+        executorService = Executors.newFixedThreadPool(100);
+        for (int i = 0; i <= (total / rows) + 1; i++) {
+            Integer index = i;
+            Integer start = i * rows;
+            Integer end = rows;
+            executorService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println(index + "-----" + start);
+                    return Thread.currentThread().getName();
+                }
+            });
+        }
+    }
 }
