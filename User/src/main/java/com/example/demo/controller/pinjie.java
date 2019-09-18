c312eb8d40396f3ac8e7b6f1eace807e034c40c8
machuangchuang <machuangchuang^@D@@G>
2019-09-18 10:50:13 +0800

    对接钉钉审批流

--- User/src/main/java/com/example/demo/controller/pinjie.java
+++ User/src/main/java/com/example/demo/controller/pinjie.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.dingtalk.api.DefaultDingTalkClient;
 import com.dingtalk.api.request.OapiGettokenRequest;
 import com.dingtalk.api.request.OapiProcessinstanceCreateRequest;
@@ -31,9 +33,7 @@ public class pinjie {
         System.out.println(response.getAccessToken());
         //创建审批流
         ProcessInstanceInputVO oapiProcessinstanceCreateRequest = new ProcessInstanceInputVO();
-//        Long deptId = 3797L;
-//        oapiProcessinstanceCreateRequest.setDeptId(deptId);
-        oapiProcessinstanceCreateRequest.setOriginatorUserId("186525515620993190");
+        oapiProcessinstanceCreateRequest.setOriginatorUserId("216110465039218508");
         // 审批流表单参数，设置各表单项值
         List<OapiProcessinstanceCreateRequest.FormComponentValueVo> formComponentValues = new ArrayList<OapiProcessinstanceCreateRequest.FormComponentValueVo>();
         OapiProcessinstanceCreateRequest.FormComponentValueVo vo1 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
@@ -48,6 +48,128 @@ public class pinjie {
         vo3.setName("行政组织");
         vo3.setValue("北京一部");
         formComponentValues.add(vo3);
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo5 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo5.setName("申请金额");
+        vo5.setValue("申请金额");
+        formComponentValues.add(vo5);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo6 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo6.setName("票据类型");
+        vo6.setValue("票据类型");
+        formComponentValues.add(vo6);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo7 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo7.setName("业务类型");
+        vo7.setValue("业务类型");
+        formComponentValues.add(vo7);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo8 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo8.setName("销售名称");
+        vo8.setValue("销售名称");
+        formComponentValues.add(vo8);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo9 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo9.setName("部门");
+        vo9.setValue("部门");
+        formComponentValues.add(vo9);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo10 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo10.setName("业绩归属");
+        vo10.setValue("业绩归属");
+        formComponentValues.add(vo10);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo11 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo11.setName("开票信息-公司名称");
+        vo11.setValue("开票信息-公司名称");
+        formComponentValues.add(vo11);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo12 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo12.setName("地址");
+        vo12.setValue("地址");
+        formComponentValues.add(vo12);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo13 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo13.setName("电话");
+        vo13.setValue("电话");
+        formComponentValues.add(vo13);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo14 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo14.setName("税号");
+        vo14.setValue("税号");
+        formComponentValues.add(vo14);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo15 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo15.setName("开户行名称");
+        vo15.setValue("开户行名称");
+        formComponentValues.add(vo15);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo16 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo16.setName("开户行账号");
+        vo16.setValue("开户行账号");
+        formComponentValues.add(vo16);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo17 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo17.setName("客户名称");
+        vo17.setValue("客户名称");
+        formComponentValues.add(vo17);
+
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo18 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo18.setName("备注");
+        vo18.setValue("备注");
+        formComponentValues.add(vo18);
+
+        // 附件
+        OapiProcessinstanceCreateRequest.FormComponentValueVo attachmentComponent = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        JSONObject attachmentJson = new JSONObject();
+        attachmentJson.put("fileId", "6433971140");
+        attachmentJson.put("fileName", "2644.JPG");
+        attachmentJson.put("fileType", "jpg");
+        attachmentJson.put("fileSize", "333");
+
+        JSONArray array = new JSONArray();
+        array.add(attachmentJson);
+        attachmentComponent.setValue(array.toJSONString());
+        attachmentComponent.setName("附件");
+        formComponentValues.add(attachmentComponent);
+        List<OapiProcessinstanceCreateRequest.FormComponentValueVo>  details = new ArrayList<OapiProcessinstanceCreateRequest.FormComponentValueVo>();
+        for(int i =0;i<1;i++){
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail1 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail1.setName("产品名称");
+            detail1.setValue("产品名称");
+            details.add(detail1);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail2 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail2.setName("信息服务费");
+            detail2.setValue("产品名称");
+            details.add(detail2);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail3 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail3.setName("信息服务费");
+            detail3.setValue("产品名称");
+            details.add(detail3);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail4 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail4.setName("接口调试费");
+            detail4.setValue("接口调试费");
+            details.add(detail4);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail5 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail5.setName("签订总金额");
+            detail5.setValue("签订总金额");
+            details.add(detail5);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail6 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail6.setName("已开票金额");
+            detail6.setValue("已开票金额");
+            details.add(detail6);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail7 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail7.setName("已回款金额");
+            detail7.setValue("已回款金额");
+            details.add(detail7);
+            OapiProcessinstanceCreateRequest.FormComponentValueVo detail8 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+            detail8.setName("本次申请金额");
+            detail8.setValue("本次申请金额");
+            details.add(detail8);
+        }
+        OapiProcessinstanceCreateRequest.FormComponentValueVo vo4 = new OapiProcessinstanceCreateRequest.FormComponentValueVo();
+        vo4.setName("明细");
+        vo4.setValue(JSON.toJSONString(Arrays.asList(details)));
+        formComponentValues.add(vo4);
         startProcessInstance(oapiProcessinstanceCreateRequest,formComponentValues,response.getAccessToken());
 
     }
@@ -59,7 +181,7 @@ public class pinjie {
             request.setFormComponentValues(formComponentValues);
             request.setProcessCode("PROC-A13A2F2E-336C-436E-B850-BCE90F850902");
             request.setOriginatorUserId(processInstance.getOriginatorUserId());
-            request.setDeptId(34429455L);
+            request.setDeptId(117704771L);
 //            request.setAgentId(277047622L);
 //            request.setDeptId(processInstance.getDeptId());
             OapiProcessinstanceCreateResponse response = client.execute(request, token);
